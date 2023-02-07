e10195fd0b90cb71dfb273e8d5514b13104f7ded
AidarUtyagulov <aidar.utyagulov98@yandex.ru>
2023-02-08 03:13:54 +0500

    add :hover

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,12 @@ const popup = document.querySelector ('.popup'); // попап окно, querySe
 const openPopupButton = document.querySelector ('.profile__button_edit'); // кнопка для показа окна
 const closePopupButton = document.querySelector ('.popup__exit'); // кнопка закрытия окна
 const getName = document.querySelector ('.profile__name'); // пункт с именем пользователя
-const getDiscription = document.querySelector ('.profile__discription'); // пункт с описанием пользователя
+const getJob = document.querySelector ('.profile__discription'); // пункт с описанием пользователя
 const editForm = document.querySelector ('.popup__container'); // окно с формой для заполнения
 
 // let - переменые, которые можно перезаписать
 let nameInput = editForm.querySelector ('.popup__input_name'); // в блоке с .popup__container → editForm, объявляем переменную для редактирования имени
-let discriptionInput = editForm.querySelector ('.popup__input_description');
+let jobInput = editForm.querySelector ('.popup__input_description');
 
 // функция для показа попап окна
 function openPopup () {
@@ -20,12 +20,13 @@ function closePopup ()  {
     popup.classList.remove ('popup__opened'); // убираем класс 
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // отменяет действие по умолчанию для события, в данном случаи отмена отправки формы (перезагрузка страницы)
-    getName.textContent = nameInput.value; // введеные пользователем данные отправляются в браузер
-    getDiscription.textContent = discriptionInput.value;
+// обработчик отправки формы
+function handleFormSubmit (evt) {
+    evt.preventDefault(); // отменяет действие по умолчанию для события, в данном случаи отмена отправки формы
+    getName.textContent = nameInput.value; // введеные пользователем данные заменяют исходное значение
+    getJob.textContent = jobInput.value;
 }
  
 openPopupButton.addEventListener ('click', openPopup); // addEventListener добавляет действие, которое сработает на клик мышки
 closePopupButton.addEventListener ('click', closePopup); // закрывает окно по клику мыши
-editForm.addEventListener ('submit', formSubmitHandler); // отправляет форму
+editForm.addEventListener ('submit', handleFormSubmit); // отправляет форму
