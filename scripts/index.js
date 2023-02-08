bc86f4fe011b6bd28584001510fb943491501450
AidarUtyagulov <aidar.utyagulov98@yandex.ru>
2023-02-08 18:43:50 +0500

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,25 @@
 // const - создаем идентификаторы (нельзя перезаписать)
 const popup = document.querySelector ('.popup'); // попап окно, querySelector принимает на вход селектор нужного элемента
-const openPopupButton = document.querySelector ('.profile__button_edit'); // кнопка для показа окна
+const openPopupButton = document.querySelector ('.profile__button_act_edit'); // кнопка для показа окна
 const closePopupButton = document.querySelector ('.popup__exit'); // кнопка закрытия окна
 const getName = document.querySelector ('.profile__name'); // пункт с именем пользователя
 const getJob = document.querySelector ('.profile__discription'); // пункт с описанием пользователя
 const editForm = document.querySelector ('.popup__container'); // окно с формой для заполнения
 
 // let - переменые, которые можно перезаписать
-let nameInput = editForm.querySelector ('.popup__input_name'); // в блоке с .popup__container → editForm, объявляем переменную для редактирования имени
-let jobInput = editForm.querySelector ('.popup__input_description');
+let nameInput = editForm.querySelector ('.popup__input_type_name'); // в блоке с .popup__container → editForm, объявляем переменную для редактирования имени
+let jobInput = editForm.querySelector ('.popup__input_type_description');
 
 // функция для показа попап окна
 function openPopup () {
-    popup.classList.add ('popup__opened'); // даем класс '.popup__opened'
+    popup.classList.add ('popup_opened'); // даем класс '.popup__opened'
+    nameInput.value = getName.textContent; // получаем данные с инпутов
+    jobInput.value = getJob.textContent;
 }
 
 // функция скрытия попап окна 
 function closePopup ()  {
-    popup.classList.remove ('popup__opened'); // убираем класс 
+    popup.classList.remove ('popup_opened'); // убираем класс 
 }
 
 // обработчик отправки формы
@@ -25,6 +27,7 @@ function handleFormSubmit (evt) {
     evt.preventDefault(); // отменяет действие по умолчанию для события, в данном случаи отмена отправки формы
     getName.textContent = nameInput.value; // введеные пользователем данные заменяют исходное значение
     getJob.textContent = jobInput.value;
+    closePopup (); // вызвал функцию для закртия при нажатии на 'сохранить'
 }
  
 openPopupButton.addEventListener ('click', openPopup); // addEventListener добавляет действие, которое сработает на клик мышки
